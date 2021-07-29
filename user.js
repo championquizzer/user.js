29bcf3a69561ba6c539f3370275591cec004c4a2
championquizzer <championquizzer@gmail.com>
2021-07-29 17:49:19 +0530

    define custom preferences for user.js

---
+++ user.js
@@ -0,0 +1,55 @@
+// Mozilla User Preferences
+
+// To change a preference value, you can either:
+// - modify it via the UI (e.g. via about:config in the browser); or
+// - set it within a user.js file in your profile.
+
+
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("beacon.enabled", false);
+user_pref("browser.bookmarks.autoExportHTML", true);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.contentblocking.category", "custom");
+user_pref("browser.contentblocking.introCount", 20);
+user_pref("browser.contentblocking.report.hide_lockwise_app", true);
+user_pref("browser.contentblocking.report.show_mobile_app", false);
+user_pref("browser.download.viewableInternally.previousHandler.alwaysAskBeforeHandling.svg", true);
+user_pref("browser.formfill.enable", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.sessionstore.privacy_level", 2);
+user_pref("browser.urlbar.searchSuggestionsChoice", true);
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("dom.security.https_only_mode", true);
+user_pref("dom.security.https_only_mode_ever_enabled", true);
+user_pref("media.autoplay.ask-permission", true);
+user_pref("media.eme.enabled", true);
+user_pref("media.navigator.enabled", false);
+user_pref("media.peerconnection.identity.timeout", 1);
+user_pref("media.peerconnection.turn.disable", true);
+user_pref("media.peerconnection.use_document_iceservers", false);
+user_pref("media.peerconnection.video.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.has-used", true);
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.firstparty.isolate", true);
+user_pref("privacy.history.custom", true);
+user_pref("privacy.purge_trackers.date_in_cookie_database", "0");
+user_pref("privacy.resistFingerprinting", true);
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("reader.color_scheme", "dark");
+user_pref("reader.content_width", 8);
+user_pref("reader.font_size", 6);
+user_pref("reader.font_type", "serif");
+user_pref("reader.line_height", 2);
+user_pref("sidebar.position_start", false);
+user_pref("webgl.disabled", true);
